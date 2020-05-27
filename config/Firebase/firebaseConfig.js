17b9a5ed3b55d690a3ce3df587fb5ca34bc1943a
Ayush Verma <vermakabir.ayush@gmail.com>
2020-05-27 16:48:03 +0530

    Add files via upload

---
+++ config/Firebase/firebaseConfig.js
@@ -0,0 +1,12 @@
+// Rename this file to "firebaeConfig.js" before use
+// Replace all Xs with real Firebase API keys
+
+export default {
+  apiKey: "AIzaSyB28Q5Osl8GJ7NQ3gjUmGrE-azCukQGD6A",
+  authDomain: "nss-iitd.firebaseapp.com",
+  databaseURL: "https://nss-iitd.firebaseio.com",
+  projectId: "nss-iitd",
+  storageBucket: "nss-iitd.appspot.com",
+  messagingSenderId: "438641610475",
+  appId: "1:438641610475:web:4967224422df1e6f2978ae"
+};
