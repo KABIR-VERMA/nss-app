340e9717e32f2ee3845d7235a4f14e3c9111c8f2
Ayush Verma <vermakabir.ayush@gmail.com>
2020-05-31 00:35:35 +0530

    Added image in Drawer

--- navigation/Drawer.js
+++ navigation/Drawer.js
@@ -1,10 +1,14 @@
-import { createDrawerNavigator} from 'react-navigation-drawer';
+import { createDrawerNavigator, DrawerItems} from 'react-navigation-drawer';
 import AppNavigation from './AppNavigation';
 import AboutUsNavigation from './AboutUsNavigation';
 import ContactUsNavigation from './ContactUsNavigation';
 import GalleryNavigation from './GalleryNavigation';
 import HoursNavigation from './HoursNavigation';
 import TeamNavigation from './TeamNavigation';
+import React ,{Component}from 'react'
+import {  SafeAreaView } from 'react-navigation';
+import {View, ScrollView, StyleSheet, Image} from 'react-native';
+
 
 const RootDrawerNavigator = createDrawerNavigator({
     Home : {
@@ -25,6 +29,23 @@ const RootDrawerNavigator = createDrawerNavigator({
     Hours:{
         screen:HoursNavigation
     }
+},{
+    contentComponent: props => (
+        <SafeAreaView style={styles.container}>
+            <View style={{height: 150, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center'}}>
+              <Image source={require('../assets/nss.jpg')} style={{height: 150, width:150, resizeMode: 'stretch'}}></Image>
+            </View>
+          <ScrollView>
+            <DrawerItems {...props} />
+          </ScrollView>
+        </SafeAreaView>
+      )
 });
 
+const styles = StyleSheet.create({
+    container: {
+      padding:30,
+      flex: 1,
+    },
+  });
 export default RootDrawerNavigator;
