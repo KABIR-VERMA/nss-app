858c9aa5c7eabb76bf1ca42b693ff8567f7f93d3
Ayush Verma <vermakabir.ayush@gmail.com>
2020-07-03 14:24:59 +0530

    Modal view edit delete Button update

--- screens/Team.js
+++ screens/Team.js
@@ -420,8 +420,8 @@ class Team extends Component {
                     }}
                   >
                     {this.myButton("Close", this.closeModal)}
-                    {this.myButton("Edit", this.editMember)}
-                    {this.myButton("Delete", this.deleteMember)}
+                    {global.isAdmin ? this.myButton("Edit", this.editMember) : null}
+                    {global.isAdmin ? this.myButton("Delete", this.deleteMember) : null}
                   </CardItem>
                 </Card>
               )}
